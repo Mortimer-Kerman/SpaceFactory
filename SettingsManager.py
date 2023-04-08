c394e739e0fe8d6839222bb1ca42cd9c02199739
Frablock <53878484+Frablock@users.noreply.github.com>
2023-04-08 18:46:36 +0200

    Ajout du resize

--- SettingsManager.py
+++ SettingsManager.py
@@ -172,7 +172,7 @@ def OpenSettings(background):
         for i in range(leak):
             name = " " + name + " "
         
-        button = SettingsMenu.add.button(name)
+        button = SettingsMenu.add.button(name).resize(100, 50,True)
         frame.pack(button, align=pygame_menu.locals.ALIGN_RIGHT)
         button.set_onreturn(lambda btn=button,kname=key:ChangeKey(btn,kname,background))
     
