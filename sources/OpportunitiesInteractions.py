56b37751d44f8793613b2c360f2e6f7e99970c00
Mortimer Kerman <29.rayko@gmail.com>
2023-04-18 16:39:51 +0200

    Commentaires

--- sources/OpportunitiesInteractions.py
+++ sources/OpportunitiesInteractions.py
@@ -110,11 +110,17 @@ class Tags:
     """
     Les différents tags d'interaction existants.
     """
+    #Est-ce que l'expédition doit utiliser un rover
     ONROVER=0#bool
+    #Est-ce que la planète a un environment lunaire
     DEADPLANET=1#bool
+    #Est-ce que la planète a un environment désertique
     DESERTICPLANET=2#bool
+    #Est-ce que la planète a un environment vivant
     LIVINGPLANET=3#bool
+    #Nombre minimal inclus de membres de l'expédition
     MINMEMBERS=4#int
+    #Nombre maximal inclus de membres de l'expédition
     MAXMEMBERS=5#int
 
 
@@ -137,21 +143,30 @@ class InteractionResult:
         Règle le moyen de transport et adapte la durée du trajet en conséquence.
         Utile pour simuler l'abandon ou la découverte d'un rover.
         """
+        #Est-ce que l'expédition utilisait déjà un rover
         alreadyOnRover = opportunity.IsOnRover()
         
+        #Si l'ancien moyen de transport et le nouveau sont identiques, inutile d'exécuter la fonction
         if alreadyOnRover == onRover:
             return
         
+        #On règle le moyen de transport dans la description de l'équipe
         opportunity.team["onRover"] = onRover
         
+        #Si l'expédition est en trajet...
         if opportunity.state == OpportunitiesManager.Opportunity.State.GOING:
-            
+            #On récupère l'avancement du trajet en faisant la durée totale moins la durée restante
             TravelAdvancement = opportunity.GetActivityDuration() - opportunity.GetLeftActivityTime()
             
+            #Si l'expédition est maintenant en rover...
             if onRover:
+                #On rapproche la date de départ en divisant par 4 la durée depuis le départ et on fait de même pour la durée prévue de l'activité
                 opportunity.SetBeginTime(datetime.now() - timedelta(seconds=TravelAdvancement//4))
                 opportunity.SetActivityDuration(opportunity.GetActivityDuration()//4)
+                
+            #Sinon, si elle se retrouve à pied...
             else:
+                #On éloigne la date de départ en multipliant par 4 la durée depuis le départ et on fait de même pour la durée prévue de l'activité
                 opportunity.SetBeginTime(datetime.now() - timedelta(seconds=TravelAdvancement*4))
                 opportunity.SetActivityDuration(opportunity.GetActivityDuration()*4)
     
@@ -167,10 +182,11 @@ class InteractionResult:
         Met directement fin à l'expédition.
         Utile pour simuler une catastrophe ayant mis fin à la vie de l'équipe ou pour simuler sa récupération par une autre équipe.
         """
-        if recoverTeam:
+        if recoverTeam:#Si l'équipe et son inventaire doivent être récupérés, cela revient à dissoudre l'expédition
             opportunity.Dissolve()
             return
         
+        #On réinitialise l'opportunité aux valeurs de départ sans transférer quoi que ce soit vers la base
         opportunity.SetState(OpportunitiesManager.Opportunity.State.PROPOSED)
         opportunity.SetActivityDuration(0)
         opportunity.returning = False
@@ -182,48 +198,64 @@ class InteractionResult:
         """
         Met l'expédtion sur le chemin du retour, indépendamment du contexte.
         """
+        #Si elle est déjà sur le chemin du retour, inutile de la rappeler
+        if opportunity.IsTravelling() and opportunity.IsReturning():
+            return
+        
+        #Si l'opportunité n'est pas en trajet...
         if not opportunity.IsTravelling():
-            opportunity.returning = True
-            opportunity.SetState(OpportunitiesManager.Opportunity.State.GOING)
-            opportunity.SetActivityDuration(opportunity.GetTravelDuration())
+            opportunity.returning = True#On lui demande de revenir
+            opportunity.SetState(OpportunitiesManager.Opportunity.State.GOING)#On la met en route
+            opportunity.SetActivityDuration(opportunity.GetTravelDuration())#On règle le temps de trajet
         else:
+            #Sinon, cela revient à la rappeler
             opportunity.Recall()
 
     def ResumeTravel(opportunity):
         """
         Permet de reprendre le trajet après une interruption.
         """
+        #On récupère la date de la dernière interruption qui est concrètement maintenant
         lastInterruption = opportunity.lastInterruption
+        #On met l'expédition en route
         opportunity.SetState(OpportunitiesManager.Opportunity.State.GOING)
+        #On remet la date de départ pour que le temps passé depuis le début soit identique à celui qu'il y avait avant l'interruption
         opportunity.SetBeginTime(datetime.now() - timedelta(seconds=lastInterruption))
 
     def DoWithChance(opportunity,chance:float,success,defeat=None):
         """
         Permet d'exécuter une fonction résultat avec une probabilité entre 0 et 1, et une autre en cas d'échet si besoin
         """
-        #random.seed(opportunity.activitySeed)
+        #Si le flottant aléatoire calculé est inférieur à la chance de succès...
         if random.uniform(0,1) < chance:
-            success(opportunity)
-        elif defeat != None:
-            defeat(opportunity)
+            success(opportunity)#On exécute la fonction de succès
+        elif defeat != None:#Sinon, si une fonction en cas d'échec est définie...
+            defeat(opportunity)#On l'exécute
     
     def AddResourceToInv(opportunity,rName:str,amount:int,amountCode:str=None):
         """
         Permet d'ajouter ou de retirer des ressources de l'inventaire de l'expédition.
         Utile pour simuler la découverte de ressources ou leur perte.
         amount peut être un entier ou un tuple pour une quantité aléatoire de resources (bornes inclues)
+        amountCode peut être utilisé pour afficher la quantité récupérée avec OpenResultPanel.
         """
+        #Si cette ressource n'est pas déjà présente dans l'inventaire, on l'initialise à 0
         if not rName in opportunity.resources:
             opportunity.resources[rName] = 0
         
+        #S la quantité est un tuple, on la règle sur une valeur aléatoire entre les deux nombres qu'elle contient
         if type(amount) == tuple:
             amount = random.randint(amount[0], amount[1])
         
+        #Si un code de quantité a été spécifié...
         if amountCode != None:
+            #On l'y ajoute à locCodes avec la quantité spécifiée
             InteractionResult.locCodes[amountCode] = amount
         
+        #On ajoute la quantité de ressources à l'inventaire
         opportunity.resources[rName] += amount
         
+        #Si la quantité de ressources finale est inférieure ou égale à 0, on efface cette ressource de l'inventaire
         if opportunity.resources[rName] <= 0:
             opportunity.resources.pop(rName)
     
@@ -231,33 +263,44 @@ class InteractionResult:
         """
         Ouvre un panneau de résultat d'interaction.
         """
+        
+        #On récupère l'écran affiché et on l'assombrit
         screenFilter = pygame.Surface((UiManager.width,UiManager.height))
         screenFilter.set_alpha(50)
         background = pygame.display.get_surface().copy()
         background.blit(screenFilter,(0,0))
+        
+        #Fonction temporaire pour afficher le fond du menu
         def DisplayBackground():
             UiManager.screen.blit(background,(0,0))
         
+        #Création du menu
         menu = pygame_menu.Menu(opportunity.GetTitle(), 800, 600, theme=pygame_menu.themes.THEME_DARK)#le thème du menu
         
+        #On récupère l'image de l'interaction et on la met à la bonne échelle avant de la mettre dans le menu
         srf = TextureManager.GetTexture(imagePath,is_menu=True)
         srf = pygame.transform.scale(srf,(550,srf.get_height()*550/srf.get_width()))
-        
         menu.add.surface(srf)
         
+        #On crée plusieurs labels pour pouvoir y mettre la description
         label = menu.add.label("\n\n\n\n\n\n",font_size=15)
         
+        #On sépare le message de l'interaction par lignes
         lines = Localization.GetLoc(message,*[InteractionResult.locCodes.get(code,code) for code in args]).split("\n")
+        #Pour chaque ligne de la description...
         for i in range(7):
+            #Si il y a une ligne correspondante dans la liste, on vient y mettre cette ligne
             if i < len(lines):
                 label[i].set_title(lines[i])
-            else:
+            else:#Sinon, cette ligne est vide
                 label[i].set_title('')
         
         menu.add.vertical_margin(5)
         
+        #On ajoute un bouton ok pour fermer le menu
         menu.add.button(Localization.GetLoc('Game.Ok'), menu.disable, font_size=20)
         
+        #Boucle du menu
         menu.mainloop(UiManager.screen, DisplayBackground)
 
 onSiteInteractions=[
@@ -325,26 +368,34 @@ def GetRandomInteractionCode(opportunity,InteractionType:int)->int:
     """
     Renvoie le code d'une interaction aléatoire.
     """
+    #Si cette interaction est une interaction sur site...
     if InteractionType in [OpportunitiesManager.InteractionType.ONSITE,OpportunitiesManager.InteractionType.ONSITERESULT]:
-        
+        #On récupère l'indice d'une interaction aléatoire dans la liste des interactions sur site
         i = random.randint(0, len(onSiteInteractions)-1)
         
+        #On précalcule les codes de description de l'opportunité pour ne pas le refaire à chaque boucle
         oppDescCodes = opportunity.GetDescCodes()
         
-        while not onSiteInteractions[i].AppliesTo(opportunity,oppDescCodes):
+        #Tant que l'interaction dont l'indice a été récupéré ne s'applique pas à cette opportunité...
+        while not onSiteInteractions[i].AppliesTo(opportunity,oppDescCodes):#On récupère un nouvel indice aléatoire
             i = random.randint(0, len(onSiteInteractions)-1)
         
+        #On renvoie cet indice
         return i
     
+    #Sinon, si c'est une interruption...
     elif InteractionType in [OpportunitiesManager.InteractionType.INTERRUPTION,OpportunitiesManager.InteractionType.INTERRUPTIONRESULT]:
-        
+        #On récupère l'indice d'une interaction aléatoire dans la liste des interactions sur site
         i = random.randint(0, len(interruptionInteractions)-1)
         
+        #On précalcule les codes de description de l'opportunité pour ne pas le refaire à chaque boucle
         oppDescCodes = opportunity.GetDescCodes()
         
-        while not interruptionInteractions[i].AppliesTo(opportunity,oppDescCodes):
+        #Tant que l'interaction dont l'indice a été récupéré ne s'applique pas à cette opportunité...
+        while not interruptionInteractions[i].AppliesTo(opportunity,oppDescCodes):#On récupère un nouvel indice aléatoire
             i = random.randint(0, len(interruptionInteractions)-1)
         
+        #On renvoie cet indice
         return i
 
 
@@ -352,7 +403,8 @@ def GetInteractionByCode(code:int,InteractionType:int)->OpportunityInteraction:
     """
     Renvoie une interaction par son type et son code.
     """
+    #Si cette interaction est une interaction sur site...
     if InteractionType in [OpportunitiesManager.InteractionType.ONSITE,OpportunitiesManager.InteractionType.ONSITERESULT]:
-        return onSiteInteractions[code]
+        return onSiteInteractions[code]#On renvoie l'interaction correspondante dans la liste des interactions sur site
     else:
-        return interruptionInteractions[code]
+        return interruptionInteractions[code]#Sinon, on cherche dans la liste des interactions d'interruptions à la place
